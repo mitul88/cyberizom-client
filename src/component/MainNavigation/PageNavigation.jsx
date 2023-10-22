5df62735b3f0ed55eececca1720432d174e333d2
mitul88 <shahed.rahman88@gmail.com>
2023-10-22 15:28:14 +0600

    react icons added, design fix

--- src/component/MainNavigation/PageNavigation.jsx
+++ src/component/MainNavigation/PageNavigation.jsx
@@ -7,11 +7,11 @@ const PageNavigation = ({handleMouseEnter, handleMouseLeave}) => {
         <div className='text-4xl font-bold'>Cyberizom</div>
         <nav className='flex gap-10 uppercase font-bold text-sm'>
             <button onMouseEnter={()=>handleMouseEnter()} onMouseLeave={()=>handleMouseLeave()} className='hover:text-red-500 ease-out duration-300'>Platform</button>
-            <button className='hover:text-red-500 ease-out duration-300'>Solutions</button>
-            <button className='hover:text-red-500 ease-out duration-300'>Partners</button>
-            <button className='hover:text-red-500 ease-out duration-300'>Company</button>
-            <button className='hover:text-red-500 ease-out duration-300'>Hackers</button>
-            <button className='hover:text-red-500 ease-out duration-300'>Resources</button>
+            <button className='hover:text-red-400 ease-out duration-300'>Solutions</button>
+            <button className='hover:text-red-400 ease-out duration-300'>Partners</button>
+            <button className='hover:text-red-400 ease-out duration-300'>Company</button>
+            <button className='hover:text-red-400 ease-out duration-300'>Hackers</button>
+            <button className='hover:text-red-400 ease-out duration-300'>Resources</button>
         </nav>
     </div>
   )
--- src/component/home/NewServiceItem.jsx
+++ src/component/home/NewServiceItem.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { BsArrowRight } from "react-icons/bs";
+
 
 const NewServiceItem = ({service}) => {
   return (
@@ -8,8 +9,9 @@ const NewServiceItem = ({service}) => {
         alt="" 
         className="rounded-t-lg w-full h-[250px]" 
         />
-        <div className="border-b border-x border-gray-300 rounded-b-lg h-[250px] flex flex-col">
-
+        <div className="border-b border-x border-gray-300 rounded-b-lg h-[250px] flex flex-col justify-between px-10 py-10">
+            <p className="font-light">{service.desc}</p>
+            <button className='w-full flex justify-between items-center text-lg text-red-400 font-bold'>{service.btnTxt}<BsArrowRight size={20} /></button>
         </div>
     </div>
   )
