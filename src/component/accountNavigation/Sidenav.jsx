5c5ea5eda11ea548dff1bf32b66802601dcbbd10
mitul88 <shahed.rahman88@gmail.com>
2023-10-22 18:24:52 +0600

    bug fix

--- src/component/accountNavigation/Sidenav.jsx
+++ src/component/accountNavigation/Sidenav.jsx
@@ -11,7 +11,7 @@ const Sidenav = () => {
     }
 
   return (
-    <div onMouseEnter={()=>handleCollapse(false)} onMouseLeave={()=>setIsCollapsed(true)} className={`min-h-screen ${isCollapsed ? 'w-[60px]' :' w-[250px] rounded-r-lg' }  bg-black text-white flex flex-col`}>
+    <div onMouseEnter={()=>handleCollapse(false)} onMouseLeave={()=>handleCollapse(true)} className={`min-h-screen ${isCollapsed ? 'w-[60px]' :' w-[250px] rounded-r-lg' }  bg-black text-white flex flex-col`}>
         <div className={`p-3 flex ${isCollapsed ?"flex-col justify-center": null} items-center text-2xl border-b border-gray-400`}>{isCollapsed? "C" : "Cyberizom"}</div>
         <div className={`${isCollapsed ? 'p-5' : 'p-2'} flex flex-col justify-center items-center`}>{isCollapsed? (<LuActivitySquare  size={30}/>) : (<button className='w-full rounded-full font-semibold p-3 hover:bg-gray-800 flex gap-5 items-center cursor-pointer'><LuActivitySquare size={30} />Hacktivity</button>)}</div>
         <div className={`${isCollapsed ? 'p-5' : 'p-2'} flex flex-col justify-center items-center`}>{isCollapsed? (<BiTargetLock size={30}/>) : (<button className='w-full rounded-full font-semibold p-3 hover:bg-gray-800 flex gap-5 items-center cursor-pointer'><BiTargetLock size={30} />Opportunities</button>)}</div>
